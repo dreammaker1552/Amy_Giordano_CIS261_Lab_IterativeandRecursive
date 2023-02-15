9cc9f0d5eb5a432cd3b7afc969b5527486a4eae2
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-15 08:40:21 -0800

    Amy Giordano

--- Amy_Giordano_CIS261_Lab_IterativeandRecursive.py
+++ Amy_Giordano_CIS261_Lab_IterativeandRecursive.py
@@ -1,6 +1,6 @@
 #Amy Giordano
 #CIS261
-#Iterative and Recursive Functionality Lab Week 9
+#Iterative and Recursive Functionality Lab Week 8
 
 
 
@@ -8,19 +8,19 @@
 # Factorial results using an iterative function
 def factorial_iterative(n):
     result = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         result *= i
     return result
-
+    
 # Factorial results using a recursive function
 def factorial_recursive(n):
     if n == 1:
         return 1
     else:
-        return n * factorial_recursive(n-1)
+        return n * factorial_recursive(n - 1)
+    for num in [0, 5, 10, 25, 50, 100]:
+        print("Factorial of", num, "using iterative function:", factorial_iterative(num))
+        print("Factorial of", num, "using recursive function:", factorial_recursive(num))
 
-# Call each function from a print statement with different input values
-for i in range(1, 6):
-    print(f"Factorial of {i} (iterative): {factorial_iterative(i)}")
-    print(f"Factorial of {i} (recursive): {factorial_recursive(i)}")
+
 
